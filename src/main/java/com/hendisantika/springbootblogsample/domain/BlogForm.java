bbc9becd775aff6cbda254959c5088100c6ca2a0
hendisantika <hendisantika@yahoo.co.id>
2019-05-19 10:55:09 +0700

    #7 - Add BlogForm Domain

---
+++ src/main/java/com/hendisantika/springbootblogsample/domain/BlogForm.java
@@ -0,0 +1,34 @@
+package com.hendisantika.springbootblogsample.domain;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-blog-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-19
+ * Time: 10:53
+ */
+public class BlogForm implements Serializable {
+
+    private Integer blogId;
+
+    @NotBlank(message = "Title Required")
+    private String title;
+
+    @NotBlank(message = "Content Required")
+    private String contents;
+
+    @NotNull(message = "Post Date Required")
+    @DateTimeFormat(pattern = "yyyy/M/d")
+    private LocalDate postDate;
+
+
+}
