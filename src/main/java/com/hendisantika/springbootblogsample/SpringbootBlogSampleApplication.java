4712f2179a2e2714899ead34f58168afff7b65c1
hendisantika <hendisantika@yahoo.co.id>
2019-05-19 10:48:39 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootblogsample/SpringbootBlogSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootblogsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBlogSampleApplication.class, args);
+    }
+
+}
