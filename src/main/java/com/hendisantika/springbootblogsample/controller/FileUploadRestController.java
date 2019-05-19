fa10b2a85204ae8194ff7709f6ce1b6a481a6fdd
hendisantika <hendisantika@yahoo.co.id>
2019-05-19 11:50:37 +0700

    #19 - Add mkdirs method in FileUploadRestController

--- src/main/java/com/hendisantika/springbootblogsample/controller/FileUploadRestController.java
+++ src/main/java/com/hendisantika/springbootblogsample/controller/FileUploadRestController.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-blog-sample
@@ -18,4 +22,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("file/upload")
 public class FileUploadRestController {
     private static final Logger logger = LoggerFactory.getLogger(FileUploadRestController.class);
+
+    /**
+     * Create a directory to store upload files
+     *
+     * @param filePath
+     * @return
+     */
+    private File mkdirs(StringBuffer filePath) {
+        Date now = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        File uploadDir = new File(filePath.toString(), sdf.format(now));
+        // Add a prefix if it already exists
+        int prefix = 0;
+        while (uploadDir.exists()) {
+            prefix++;
+            uploadDir =
+                    new File(filePath.toString() + sdf.format(now) + "-" + prefix);
+
+        }
+
+        // Create folder
+        uploadDir.mkdirs();
+
+        return uploadDir;
+    }
 }
